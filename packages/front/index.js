d34b1b5c76b8d4f9a567e136f4fce6af519a54c3
AugustoPeraltaSoft <82390234+AugustoPeraltaSoft@users.noreply.github.com>
2021-04-12 12:04:15 -0300

    feat: change on the front package

--- packages/front/index.js
+++ packages/front/index.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   // TODO: say hi, again and again and more
-    console.log("BEHOLD MY FRONT PACKAGE!");
+    console.log("BEHOLD MY FRONT PACK!");
     console.log("hello peoplee, from @gskTest/common");
     console.log("hello from @gskTest/common again");
 };
